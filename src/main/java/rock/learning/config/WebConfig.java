aa2f1bebb997f7dd504f5ebdd09e8c84e7514a1d
Rocktim <rajkumar.rocktim@gmail.com>
2019-07-27 19:34:47 +0530

    java > config >WebConfig : annotation EnableMVC,Configuration,ComponentScan done

--- src/main/java/rock/learning/config/WebConfig.java
+++ src/main/java/rock/learning/config/WebConfig.java
@@ -1,4 +1,11 @@
 package rock.learning.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "rock.learning")
 public class WebConfig {
 }
