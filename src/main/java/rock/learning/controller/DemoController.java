b086bf6f328a8c9f8804c596454c0a4b42899ee4
Rocktim <rajkumar.rocktim@gmail.com>
2019-07-29 08:45:31 +0530

    java > controller >DemoController : GetMapping(/hello) added

--- src/main/java/rock/learning/controller/DemoController.java
+++ src/main/java/rock/learning/controller/DemoController.java
@@ -1,4 +1,14 @@
 package rock.learning.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DemoController {
+
+    //http://locahost:8080/todo-list/hello
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
 }
