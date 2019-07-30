21aeeddf724764305fdbd5e1be3d35b8c47fbdf2
Rocktim <rajkumar.rocktim@gmail.com>
2019-07-30 08:43:48 +0530

    java > config > WebConfig : ViewResolver added

--- src/main/java/rock/learning/controller/DemoController.java
+++ src/main/java/rock/learning/controller/DemoController.java
@@ -2,13 +2,23 @@ package rock.learning.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DemoController {
 
     //http://locahost:8080/todo-list/hello
+    @ResponseBody
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    //http://localhost:8080/todo-list/welcome
+    //prefix + name + suffix
+    //WEB-INF/view/welcome.jsp
+    @GetMapping("welcome")
+    public String welcome(){
+        return "welcome";
     }
 }
