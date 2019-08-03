55afb9a9ee2242cb09ef406f8bfc5652e4540014
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-03 15:17:10 +0530

    java > model > TodoItem : boilerplate code remove by adding lombok Data annotation

--- src/main/java/rock/learning/model/TodoItem.java
+++ src/main/java/rock/learning/model/TodoItem.java
@@ -1,7 +1,10 @@
 package rock.learning.model;
 
+import lombok.Data;
+
 import java.time.LocalDate;
 
+@Data
 public class TodoItem {
 
     // ==== Fields ====
@@ -15,62 +18,5 @@ public class TodoItem {
         this.title = title;
         this.detais = detais;
         this.deadline = deadline;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDetais() {
-        return detais;
-    }
-
-    public void setDetais(String detais) {
-        this.detais = detais;
-    }
-
-    public LocalDate getDeadline() {
-        return deadline;
-    }
-
-    public void setDeadline(LocalDate deadline) {
-        this.deadline = deadline;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof TodoItem)) return false;
-
-        TodoItem todoItem = (TodoItem) o;
-
-        return id == todoItem.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return id;
-    }
-
-    @Override
-    public String toString() {
-        return "TodoItem{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", detais='" + detais + '\'' +
-                ", deadline=" + deadline +
-                '}';
     }
 }
