d502ff73fa11e57fe0ec9ef9019a38203618ed40
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-04 18:08:01 +0530

    java > controller > TodoItemController : todoItemService implemented

--- src/main/java/rock/learning/controller/TodoItemController.java
+++ src/main/java/rock/learning/controller/TodoItemController.java
@@ -1,19 +1,31 @@
 package rock.learning.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import rock.learning.model.TodoData;
+import rock.learning.service.TodoItemService;
 import rock.learning.util.Mappings;
 import rock.learning.util.ViewNames;
 
 @Controller
 public class TodoItemController {
 
+    //==== Fields ====
+    private final TodoItemService todoItemService;
+
+    // === constructor ====
+
+    @Autowired
+    public TodoItemController(TodoItemService todoItemService) {
+        this.todoItemService = todoItemService;
+    }
+
     // === model attributes ===
     @ModelAttribute
     public TodoData todoData(){
-        return new TodoData();
+        return todoItemService.getData();
     }
 
     // ===== handler method ====
