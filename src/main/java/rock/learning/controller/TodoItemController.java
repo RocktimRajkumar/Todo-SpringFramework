24ecf4382c39ad9cdfea6ffe1dbaa7c9ae8bfddf
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-04 11:32:28 +0530

    java > controller > TodoItemController : model attributes todoData and handler method items done

--- src/main/java/rock/learning/controller/TodoItemController.java
+++ src/main/java/rock/learning/controller/TodoItemController.java
@@ -1,4 +1,23 @@
 package rock.learning.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import rock.learning.model.TodoData;
+
+@Controller
 public class TodoItemController {
+
+    // === model attributes ===
+    @ModelAttribute
+    public TodoData todoData(){
+        return new TodoData();
+    }
+
+    // ===== handler method ====
+    // === http://localhost:8080/todo-list/items
+    @GetMapping("items")
+    public String items(){
+        return "items";
+    }
 }
